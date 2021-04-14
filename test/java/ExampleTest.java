5abe8a766d23d320c4bc82f4c638b8f5ca68f913
Duarte Carvalho <up201503661@fe.up.pt>
2021-04-14 19:29:33 +0100

    Created tests folder and configuration, added an example test

---
+++ test/java/ExampleTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ExampleTest {
+
+    @Test
+    public void example() {
+        int x = 0, y = 0;
+
+        assertEquals(x,y);
+    }
+}
