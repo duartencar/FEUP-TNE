064532b73114c485c6a292b4c24c0df2e23ec141
duartencar <up201503661@fe.up.pt>
2021-04-14 23:59:12 +0100

    Created Constants class
    Closes #25

---
+++ src/utils/Constants.java
@@ -0,0 +1,10 @@
+package utils;
+
+public final class Constants {
+
+    public final class Arguments {
+        public static final int ARGUMENT_GUI_INDEX = 0;
+        public static final int ARGUMENT_MAP_INDEX = 1;
+        public static final int ARGUMENT_AGENTS_INDEX = 2;
+    }
+}
