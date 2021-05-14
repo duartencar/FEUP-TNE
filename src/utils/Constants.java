f4337831f7550434ada60abcb079823a4bff6890
Duarte Carvalho <up201503661@fe.up.pt>
2021-05-14 19:30:11 +0100

    Added file paths

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -7,4 +7,10 @@ public final class Constants {
         public static final int ARGUMENT_MAP_INDEX = 1;
         public static final int ARGUMENT_AGENTS_INDEX = 2;
     }
+
+    public final class Directories {
+        public static final String MAPS_PATH = "../data/maps/";
+        public static final String SIMULATIONS_PATH = "../data/simulations/";
+        public static final String RESULTS_PATH = "../data/results/";
+    }
 }
