749306a215d08b3b39eeaccade89f7d8fa0647aa
Duarte Carvalho <up201503661@fe.up.pt>
2021-06-06 16:06:08 +0100

    started complex agents

---
+++ src/agents/ComplexRequestAgent.java
@@ -0,0 +1,9 @@
+package agents;
+
+public class ComplexRequestAgent extends RequestAgent {
+
+    public ComplexRequestAgent(int id, String name, String randomRequests, String fileOrNumberOfRequests, char heuristic) throws Exception {
+        super(id, name, randomRequests, fileOrNumberOfRequests, heuristic);
+    }
+    
+}
---
+++ src/agents/ComplexVehicle.java
@@ -0,0 +1,9 @@
+package agents;
+
+import map.GraphNode;
+
+public class ComplexVehicle extends Vehicle {
+    public ComplexVehicle(String n, String t, GraphNode sp, float ts, float mc) {
+        super(n, t, sp, ts, mc);
+    }
+}
